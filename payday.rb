60e4c63a2513298d26bcfff870feaf12338a87dd
norbx <norsku27@yahoo.com>
2020-12-19 14:47:37 +0100

    Fix payday.rb

--- payday.rb
+++ payday.rb
@@ -1,6 +1,6 @@
 require './config/application'
 
-data = Preprocessor.new(Reader.read_csv('./wydatki_short.csv')).extract_dates
+data = Preprocessor.new(Reader.read_csv('./wydatki_short.csv'), '2020-10-23', '2020-10-26').extract_dates
 
 Reports.new(data, date_from: '2020-10-23', date_to: '2020-10-26').create_report
 
