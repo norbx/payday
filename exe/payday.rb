d3ce8f2b5cf0d5464a798d4690e14a046920070f
norbx <norsku27@yahoo.com>
2021-02-20 01:04:11 +0100

    Change hardcoded date_from and date_to to inputtable

--- exe/payday.rb
+++ exe/payday.rb
@@ -1,7 +1,16 @@
 require './config/application'
 
-data = Preprocessor.new(Reader.read_csv('./wydatki_short.csv'), '2020-10-23', '2020-10-25').extract_dates
+puts 'Enter file path:'
+file_path = gets.chomp
 
-Reports.new(data, date_from: '2020-10-23', date_to: '2020-10-25').create_report
+puts 'Enter previous payday date:'
+date_from = gets.chomp
+
+puts 'Enter current payday date:'
+date_to = gets.chomp
+
+data = Preprocessor.new(Reader.read_csv(file_path), date_from, date_to).extract_dates
+
+Reports.new(data, date_from: date_from, date_to: date_to).create_report
 
 sh 'rake c'
