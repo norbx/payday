6d82fdb9b2b27b98911eab76ae353469dd063790
norbx <norsku27@yahoo.com>
2021-03-06 00:10:37 +0100

    Add test exe file, gitignore the rest

--- exe/payday.rb
+++ exe/payday.rb
@@ -6,16 +6,16 @@ load './db/seeds.rb'
 puts 'Enter file path:'
 file_path = gets.chomp
 
-# puts 'Enter previous payday date:'
-# date_from = gets.chomp
-#
-# puts 'Enter current payday date:'
-# date_to = gets.chomp
+puts 'Enter previous payday date:'
+date_from = gets.chomp
+
+puts 'Enter current payday date:'
+date_to = gets.chomp
 
 data = Preprocessor.new(Reader.read_csv(file_path)).extract_dates
 
 Importer.new(data).import
 
-Reports.new(data, date_from: '2021-01-04', date_to: '2021-02-12').create_report
+Reports.new(data, date_from: date_from, date_to: date_to).create_report
 
 sh 'rake c'
