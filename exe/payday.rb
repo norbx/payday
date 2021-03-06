c03b5de814327b9cea16eb2dad024d2b00960651
norbx <norsku27@yahoo.com>
2021-03-06 16:33:55 +0100

    Fix executable

--- exe/payday.rb
+++ exe/payday.rb
@@ -3,19 +3,4 @@ require './config/application'
 
 load './db/seeds.rb'
 
-puts 'Enter file path:'
-file_path = gets.chomp
-
-puts 'Enter previous payday date:'
-date_from = gets.chomp
-
-puts 'Enter current payday date:'
-date_to = gets.chomp
-
-data = Preprocessor.new(Reader.read_csv(file_path)).extract_dates
-
-Importer.new(data).import
-
-Reports.new(data, date_from: date_from, date_to: date_to).create_report
-
-sh 'rake c'
+require './lib/views/main_menu'
