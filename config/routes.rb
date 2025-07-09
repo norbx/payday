4570669c0d2d8c7f76e7979bc1cf1cd6feb5bde6
norbert <norsku27@yahoo.com>
2025-07-09 13:23:15 +0200

    set root view to categorise#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :expenses_imports, only: [ :index, :create ], path: :imports, as: :imports, controller: :imports
   resource :categorise, only: [ :show, :create ], controller: :categorise
 
+  root "categorise#show"
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
