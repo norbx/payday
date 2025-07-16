8a7edd8a10ded821bf2af9504342095eac9d7169
norbert <norsku27@yahoo.com>
2025-07-16 12:41:11 +0200

    refactor ImportExpenses service

--- app/services/import_expenses.rb
+++ app/services/import_expenses.rb
@@ -16,58 +16,62 @@ class ImportExpenses < BaseService
   def initialize(csv_file, filename)
     @csv_file = csv_file
     @filename = filename
+
+    @key_mapping = nil
+    @import ||= ExpensesImport.create!(file_name: @filename)
+    @smarter_csv ||= File.open(@csv_file, "r:bom|utf-8") do |f|
+      SmarterCSV.process(f, chunk_size: 1000)
+    end
   end
 
   def call
     Expense.transaction do
-      @import ||= ExpensesImport.create!(file_name: @filename)
-      @smarter_csv ||= File.open(@csv_file, "r:bom|utf-8") do |f|
-        SmarterCSV.process(f, chunk_size: 1000)
-      end
-
-      if mbank_csv?
-        @smarter_csv.each do |chunk|
-          expenses = chunk.map { csv_to_hash(_1, MBANK_KEYS) }
-          Expense.insert_all(expenses, unique_by: :expenses_unique_index)
-        end
-      elsif pkobp_csv?
-        @smarter_csv.each do |chunk|
-          expenses = chunk.map { csv_to_hash(_1, PKOBP_KEYS) }
-          Expense.insert_all(expenses, unique_by: :expenses_unique_index)
-        end
-      else
-        raise I18n.t("imports.unsupported_format")
-      end
-
-      @import.update(state: "completed")
+      @key_mapping = set_key_mapping
+      @smarter_csv.each { |chunk| map_and_save_expenses(chunk) }
+      @import.update(state: :completed)
     end
   end
 
   private
 
-  def csv_to_hash(expense, key_mapping)
-    subcategory_name = key_mapping == PKOBP_KEYS ? map_category(expense, key_mapping) : expense[key_mapping[:category]]
+  def set_key_mapping
+    if mbank_headers?
+      MBANK_KEYS
+    elsif pkobp_headers?
+      PKOBP_KEYS
+    else
+      raise I18n.t("imports.unsupported_format")
+    end
+  end
+
+  def map_and_save_expenses(chunk)
+    expenses = chunk.map { csv_to_hash(_1) }
+    Expense.insert_all(expenses, unique_by: :expenses_unique_index)
+  end
+
+  def csv_to_hash(expense)
+    subcategory_name = @key_mapping == PKOBP_KEYS ? map_category(expense) : expense[@key_mapping[:category]]
     subcategory = Subcategory.find_by_name(name: subcategory_name)
 
     {
-      date: expense[key_mapping[:date]],
-      description: expense[key_mapping[:description]].squeeze(" ")|| "Brak opisu",
-      amount: expense[key_mapping[:amount]].to_f,
+      date: expense[@key_mapping[:date]],
+      description: expense[@key_mapping[:description]].squeeze(" ")|| "Brak opisu",
+      amount: expense[@key_mapping[:amount]].to_f,
       category_id: subcategory.category.id,
       subcategory_id: subcategory.id,
       expenses_import_id: @import.id
     }
   end
 
-  def map_category(expense, key_mapping)
-    CategoryMapper::PKOBP_KEY_MAPPING[expense[key_mapping[:category]]]
+  def map_category(expense)
+    CategoryMapper::PKOBP_KEY_MAPPING[expense[@key_mapping[:category]]]
   end
 
-  def mbank_csv?
+  def mbank_headers?
     (MBANK_KEYS.values - @smarter_csv.flatten.first.keys).empty?
   end
 
-  def pkobp_csv?
+  def pkobp_headers?
     (PKOBP_KEYS.values - @smarter_csv.flatten.first.keys).empty?
   end
 end
