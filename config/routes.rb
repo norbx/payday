8b2c4e13a67b442e717331f91b94701e29f1ec56
norbert <norsku27@yahoo.com>
2025-07-16 12:56:00 +0200

    switch root route to imports#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :expenses_imports, only: [ :index, :create ], path: :imports, as: :imports, controller: :imports
   resource :categorise, only: [ :show, :create ], controller: :categorise
 
-  root "categorise#show"
+  root "imports#index"
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
